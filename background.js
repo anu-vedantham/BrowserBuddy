6aafaca20ca94bae239e0d0d4082e613ece86807
nunu0129 <avedanth@andrew.cmu.edu>
2013-08-09 13:45:25 -0700

    can test gif animations now

--- background.js
+++ background.js
@@ -1,25 +1,22 @@
 var alarmName = "BB"
 
-$(document).ready(function(){
-	chrome.storage.sync.clear();
+	//chrome.storage.sync.clear();
 	var dict = {
 		'opacity': 1,
 		'greyscale': 0,
-		'expression': 'testing.png',
+		'expression': 'bunny_happy.png',
 		'checked':false
 	};
 	chrome.storage.sync.set(dict);
-})
 
-chrome.storage.onChanged.addListener(function(changes, areaName) {
-	if (changes.checked !== null){
+/*chrome.storage.onChanged.addListener(function(changes, areaName) {
+	var t = 5;
+	if (changes.checked !== null && changes.checked !== undefined){
 		if (changes.checked === true){
+			console.log("debug");
 			alarmInfo = {'name':bs.alarmName, 
 			'when':Date.now(), 'periodInMinutes':1};
 			chrome.alarms.create(alarmName, alarmInfo);
-		}
-		else{
-			chrome.alarms.clear(alarmName);
 		}
 	} 
 
@@ -27,9 +24,9 @@ chrome.storage.onChanged.addListener(function(changes, areaName) {
 
 chrome.alarms.onAlarm.addListener(function(alarm) {
 	chrome.storage.get(null, function(items){
-		alert('buddy is more sad');
+		console.log('bunny is more sad');
 		op = items.opacity;
 		gr = items.greyscale;
 		chrome.storage.set({'opacity':op-0.3, 'greyscale':gr-0.3});
 	 });
-});
+});*/
--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-var buddySrc = "testing.png";
+var buddySrc = "bunny_happy.png";
 
 function updateGreyScale(newVal){
 	img = $("#buddy")[0];
@@ -9,33 +9,110 @@ function updateGreyScale(newVal){
 }
 
 function getStoredValues(items){
-	if (items != null){
+	if (items != null && items !== undefined){
 		stored = $.extend(true, {}, items);
-		$("#fadeOut").css("opacity",items.opacity);
-		updateGreyScale($("#fadeOut")[0], items.greyscale);
+		//set the opacity
+		$("#buddy").css("opacity",items.opacity);
+		buddySrc = items.expression;
+		//set the greyscale
+		updateGreyScale(items.greyscale);
+		if (items.checked){
+			$('#page').prop("checked", true);
+		}
+		else
+			$('#page').prop("checked", false);
+
 	}
 	else
 		console.log('WE HAVE A PROBLEM');
 }
 
-(document).ready(function() {
-		var dict = {
-				'opacity': 1,
-				'greyscale': 0,
-				'expression': 'testing.png',
-				'checked':false
-			};
+function changeExpression(op, gr){
+	if (op >= 0.7 && gr <= 0.3) //[0.7,1] & [0, 0.3]
+		buddySrc = "bunny_happy.png";
+	else if(op < 0.5 && gr > 0.6) //[0, 0.5) & (0.6,1]
+		buddySrc = "bunny_down.png";
+	else //[0.5, 0.7) & (0.3, 0.6]
+		buddySrc = "bunny_sad.png";
+	updateGreyScale(gr);
+}
 
-		chrome.storage.sync.get(dict, getStoredValues);
-		$("#page").click(function(){
+/*chrome.storage.onChanged.addListener(function(changes, areaName) {
+	if (changes.opacity !==null && changes.opacity !== undefined){
+		$("#buddy").css("opacity", changes.opacity);
+	}
+	if (changes.expression !== null && changes.expression !== undefined){
+		strGr = $('#buddy').attr('data-pb-greyscale-opacity');
+		currGr = parseFloat(strGr);
+		buddySrc = changes.expression; 
+		if (changes.greyscale !== null && changes.greyscale !== undefined){
+			updateGreyScale(changes.gr);
+		}
+		updateGreyScale(currGr);	
+	}
+});*/
+
+$(document).ready(function() {
+	var dict = {
+			'opacity': 1,
+			'greyscale': 0,
+			'expression': 'bunny_happy.png',
+			'checked':false
+		};
+	var incr = 0.1;
+	/*Hey Dave! change this variable to test
+	chrome.storage.sync.set({'expression':''})*/
+
+	chrome.storage.sync.get(dict, getStoredValues);
+	$("#page").click(function(){
+		if ( $(this).is(':checked') )
 			chrome.storage.sync.set({'checked':true});
-		});
-		chrome.storage.onChanged.addListener(function(changes, areaName) {
-			if (changes.opacity !==null){
-				$("#buddy").css("opacity",changes.opacity);
-			}
-			if (changes.greyscale !== null){
-				updateGreyScale()
-			}
-		});
+		else
+			chrome.storage.sync.set({'checked':false});
+
+	});
+
+	$("#op_up").click(function(){
+		strOp = $('#buddy').css('opacity');
+		currOp = parseFloat(strOp);		
+		if ((currOp+incr <= 1)){
+			newOp = currOp+incr;
+			$("#buddy").css("opacity",newOp);
+			chrome.storage.sync.set({'opacity':newOp});
+		}
+	});
+
+	$("#op_down").click(function(){
+		strOp = $('#buddy').css('opacity');
+		currOp = parseFloat(strOp);
+		if ((currOp-incr >= 0)){
+			newOp = currOp-incr;
+			$("#buddy").css("opacity",newOp);
+			chrome.storage.sync.set({'opacity': newOp});
+		}
+	});
+
+	$("#sat_up").click(function(){
+		//Saturation up = Greyscale down
+		strGr = $('#buddy').attr('data-pb-greyscale-opacity');
+		currGr = parseFloat(strGr);
+		if (currGr-incr >= 0){
+			newGr = currGr-incr;
+			updateGreyScale(newGr);
+			chrome.storage.sync.set({'greyscale':newGr});
+		}
+	});
+
+	$("#sat_down").click(function(){
+		//Saturation down = Greyscale up
+		strGr = $('#buddy').attr('data-pb-greyscale-opacity');
+		currGr = parseFloat(strGr);
+		if (currGr+incr <= 1){
+			newGr = currGr+incr;
+			updateGreyScale(newGr);
+			chrome.storage.sync.set({'greyscale':newGr});
+		}
+	});
+		
 });
+	
